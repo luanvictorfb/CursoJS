44762c3ddf0b37c8f3922ca1e4af1bca131e7da5
Luan Victor de Faria Brito <luanvct2003@gmail.com>
2025-02-11 11:07:20 -0300

    11/01

--- aula20/script.js
+++ aula20/script.js
@@ -1,16 +1,32 @@
-let res = document.querySelector('div#res')
-let txtn = document.querySelector('input#txtn')
+let num = document.querySelector('input#txtn')
 let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
 let array = []
 
-function adicionar() {
-
-    if (isNumero(Number.value) && !inLista(Number.value)) {
-        
-    } else {
-        window.alert('Número não válido ou já encontrado na lista')
+function isNum(n){
+    if(Number(n)>=1 && Number(n)<=100){
+        return true
     }
-
-    txtn.value = ""
-    txtn.focus()
+    else{
+        return false
+    }
+}
+function inLista(n,l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }
+    else{
+        return false
+    }
+}
+function adicionar() {
+    if(isNum(num.value) && !inLista(num.value, array)) {
+        array.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+    }
+    else{
+        window.alert('Número não válido ou já encontrado na lista.')
+    }
 }
